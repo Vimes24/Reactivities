4dfb7f5ece8ecac1c82ffb48cbc2a9e04dc244cc
Wojciech Skrzypek <skrzypek.wojciech@gmail.com>
2023-06-28 20:24:26 +0200

    Poprawki w widoku interfejsu użytkownika

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect } from 'react';
-import { Button, Card, Grid, Image } from 'semantic-ui-react';
+import { useEffect } from 'react';
+import { Grid } from 'semantic-ui-react';
 import { useStore } from '../../../app/stores/store';
 import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { observer } from 'mobx-react-lite';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import ActivityDetailedHeader from './ActivityDetailedHeader';
 import ActivityDetailedInfo from './ActivityDetailedInfo';
 import ActivityDetailedChat from './ActivityDetailedChat';
@@ -24,8 +24,8 @@ export default observer(function ActivityDetails(){
     return(
         <Grid>
             <Grid.Column width={10}>
-                <ActivityDetailedHeader />
-                <ActivityDetailedInfo />
+                <ActivityDetailedHeader activity={activity} />
+                <ActivityDetailedInfo activity={activity}/>
                 <ActivityDetailedChat />
             </Grid.Column>
             <Grid.Column width={6}>
