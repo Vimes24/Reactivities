18d3ebc0defc7d8c092d425bf9c309f34aeecc0c
Wojciech Skrzypek <skrzypek.wojciech@gmail.com>
2023-07-02 09:38:28 +0200

    Poprawki i validacja formularza dodawania wydarzeń

---
+++ client-app/src/app/common/options/categoryOptions.ts
@@ -0,0 +1,8 @@
+export const categoryOptions = [
+    {text: 'Alkohol', value: 'drinks'},
+    {text: 'Film', value: 'film'},
+    {text: 'Jedzenie', value: 'food'},
+    {text: 'Kultura', value: 'culture'},
+    {text: 'Muzyka', value: 'music'},
+    {text: 'Podróż', value: 'travel'},
+]
